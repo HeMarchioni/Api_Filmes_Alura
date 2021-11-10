ee647d524d24973824fda7b23b4218f2ac7dc443
ModalGR <ModalGR@DESKTOP-DSMS57L>
2021-11-10 16:20:35 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,110 @@
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]  // -> define que é um controlador
+    [Route("[controller]")] // -> Define a rota mesmo nome que a [controller] (Filme)
+    public class FilmeController : ControllerBase //-> sempre extender a classe Base
+    {
+
+        private FilmeContext _context;  //-> Variavel que recebe -> a classe de acesso ao BD
+
+
+        public FilmeController(FilmeContext filmeContext)   //-> injetando o FilmeContect(DB) no contrutor
+        {
+            _context = filmeContext;
+        }
+
+
+
+
+        [HttpPost]  //-> metodo http que ira Fazer a chamada da rota
+        public IActionResult AdicionaFilme([FromBody] Filme filme)  //-> FromBody (vem do corpo da requisição)
+        {
+
+            _context.Add(filme); //-> _context é a extensao de acesso ao banco .add (adiciona)
+
+            _context.SaveChanges();   // ->depois de alterar algo tem que salvar as mudanças 
+
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme); // -> retorno para elemento criado
+
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()  //-> IEnuerable (interface define que pode ser o retorno qualquer tipo List, set, )
+        {
+            return _context.Filmes;   //   _context.Filmes -> vai no banco e volta todos os filmes
+        }
+
+
+        [HttpGet("{id}")] // -> passa o valor pela Url e passa para Variavel de Entrada
+        public IActionResult RecuperaFilmesPorId(int id)  //->recebe o Valor da Url // -> IActionResult (retorna um resultado 200 ok 404 (nesse caso Created)
+        {
+
+
+
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id); // -> (FirstOrDefault) Retorna o primeiro elemento da sequência que satisfaz uma condição ou um valor padrão
+
+            if (filme != null)
+            {
+                return Ok(filme); // -> Retorno para (Ok 200)
+            }
+            return NotFound(); // -> retorno de Nao encontrado (404)
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilmePorId(int id, [FromBody] Filme filmeAtualizado)  //->recebe o Valor da Url // -> IActionResult (retorna um resultado 200 ok 404 (nesse caso Created)
+        {
+
+
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+
+            if (filme == null) // busca pelo id se for nulo 
+            {
+                return NotFound(); // -> Retorno nao encontrado (404)
+            }
+
+            filme.Titulo = filmeAtualizado.Titulo;
+            filme.Genero = filmeAtualizado.Genero;
+            filme.Duracao = filmeAtualizado.Duracao;
+            filme.Diretor = filmeAtualizado.Diretor;
+
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarFilmePorId(int id)  //->recebe o Valor da Url // -> IActionResult (retorna um resultado 200 ok 404 (nesse caso Created)
+        {
+
+
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id); // -> (FirstOrDefault) Retorna o primeiro elemento da sequência que satisfaz uma condição ou um valor padrão
+
+            if (filme != null)
+            {
+
+                _context.Remove(filme);   // -> remover o filme do BD
+
+                _context.SaveChanges(); // -> salvar mudanças
+
+                return NoContent(); // -> Retorno  (Ok 204 sem resposta)
+            }
+            return NotFound(); // -> retorno de Nao encontrado (404)
+
+
+        }
+    }
+}
+
+
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]  //-> PK
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O Campo titulo é Obrigatorio")]  //-> Validação (ja retorna Bad request e a mensagem no lugar do OBJ) 
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Campo Diretor é Obrigatorio")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage =" O genero nao pode passar de 30 Caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1,600, ErrorMessage = "A duração deve ter no minimo 1 a 600 Minutos")] //->range
+        public float Duracao { get; set; }
+
+
+    }
+}
